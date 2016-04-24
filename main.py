bd898392b6bfcf8a331c6356d693945c06ca3298
DeastinY <richard.polzin@gmail.com>
2016-04-24 18:28:00 +0200

    implemented Get Lucky button

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ kivy.require('1.9.0')
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+class AttrWidget(GridLayout):
+    def UpdateAll(self):
+        app = App.get_running_app()
+        app.Char.RandAll()
+        self.ids['b_name'].text=app.Char.Name
+        self.ids['b_age'].text=app.Char.Age
+        self.ids['b_metatype'].text=app.Char.Metatype
+        self.ids['b_gender'].text=app.Char.Gender
+        self.ids['b_trait'].text=app.Char.Traits
+        self.ids['b_special'].text=app.Char.Special
 
 class SRNPCGen(App):
     def __init__(self):
