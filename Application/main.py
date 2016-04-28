b99c288da502a0aac529685ec9392b37084e15ec
DeastinY <richard.polzin@gmail.com>
2016-04-28 22:17:53 +0200

    added nav to python code so menu switch can be done in code behind

--- Application/main.py
+++ Application/main.py
@@ -15,48 +15,18 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.metrics import dp
+from kivy.graphics import *
 
 from kivy.garden.navigationdrawer import NavigationDrawer
+
+class CustomNavigationDrawer(NavigationDrawer):
+    pass
 
 class SRNPCGen(App):
     def __init__(self):
         super(SRNPCGen, self).__init__()
         self.initialize_accelerometer()
-
-    def build(self):
-        self.navigationdrawer = NavigationDrawer()
-        self.side_panel = GridLayout(cols=1,rows=3)
-        self.main_panel = GridLayout(cols=1,rows=2, padding=[0,10,0,0])
-        m = self.main_panel
-        nav = self.navigationdrawer
-        s = self.side_panel
-
-        nav.anim_type = 'slide_above_anim'
-
-        s.add_widget(Button(text='Generate', height=70, size_hint_y=None))
-        s.add_widget(Button(text='Database', height=70, size_hint_y=None))
-        text = Label(text='Hier würde jetzt cooler text stehen, wenn mir denn was einfallen würde. \
-                     Oder ein Bild, oder so. kA. Wenn ihr ideen habt, nur raus damit :D ')
-        text.text_size=(text.width,None)
-        text.valign='middle'
-        text.halign='center'
-        s.add_widget(text)
-
-        top_bar = GridLayout(cols=2, rows=1, height=50, size_hint_y=None, padding=[10,0,10,0])
-        top_bar.add_widget(Button(text="Menu", on_press=self.toggle_drawer, width=50, size_hint_x=None))
-        top_bar.add_widget(Label(text = "Roll Your NPC", font_size=30, italic=True))
-
-        m.add_widget(top_bar)
-        m.add_widget(RandChar())
-        # First : Side, Second : Main
-        nav.add_widget(s)
-        nav.add_widget(m)
-
-        return nav
-
-    def toggle_drawer(self, sender):
-        self.navigationdrawer.toggle_state()
-
+        self.main = None
 
     def initialize_accelerometer(self):
             if accelerometer.enable():
@@ -67,6 +37,28 @@ class SRNPCGen(App):
                 print("Accelerometer not available")
                 self.has_accelerometer = False
 
+    def build(self):
+        self.nav = CustomNavigationDrawer()
+        self.open_generator()
+        return self.nav
+
+    def menu_btn_pressed(self):
+        btns = [self.nav.ids.btn_generate,self.nav.ids.btn_database]
+        for b in btns:
+            b.color=[1,1,1,1]
+            b.font_size=15
+
+    def open_generator(self):
+        if not self.main is RandChar:
+            self.highlight_btn(self.nav.ids.btn_generate)
+            if self.main:
+                self.nav.ids.main.remove_widget(self.main)
+            self.main = RandChar()
+            self.nav.ids.main.add_widget(self.main)
+
+    def highlight_btn(self, btn):
+        btn.color=[0,1,0,1]
+        btn.font_size=20
 
     def on_pause(self):
         Clock.unschedule(self.get_acceleration)
